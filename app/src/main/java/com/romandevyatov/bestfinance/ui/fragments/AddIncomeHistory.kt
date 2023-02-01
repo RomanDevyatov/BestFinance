7ec63c6e2896e7254f224608f18e3d36be112c59
Roman Devyatov <roman_devyatov@epam.com>
2023-02-01 03:37:02 +0300

    added date picker, added wallet dropdown

--- app/src/main/java/com/romandevyatov/bestfinance/ui/fragments/AddIncomeHistory.kt
+++ app/src/main/java/com/romandevyatov/bestfinance/ui/fragments/AddIncomeHistory.kt
@@ -1,5 +1,7 @@
 package com.romandevyatov.bestfinance.ui.fragments
 
+import android.app.DatePickerDialog
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,12 +9,15 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.romandevyatov.bestfinance.R
 import com.romandevyatov.bestfinance.databinding.FragmentAddIncomeHistoryBinding
 import com.romandevyatov.bestfinance.viewmodels.IncomeGroupViewModel
+import com.romandevyatov.bestfinance.viewmodels.WalletViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 @AndroidEntryPoint
@@ -20,6 +25,7 @@ class AddIncomeHistory : Fragment() {
 
     private lateinit var binding: FragmentAddIncomeHistoryBinding
     private val incomeGroupViewModel: IncomeGroupViewModel by viewModels()
+    private val walletViewModel: WalletViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,13 +33,14 @@ class AddIncomeHistory : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentAddIncomeHistoryBinding.inflate(inflater, container, false)
+
+        initIncomeGroupSpinner()
+        initWalletSpinner()
+
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding = FragmentAddIncomeHistoryBinding.bind(view)
-
+    private fun initIncomeGroupSpinner() {
         val spinnerAdapter = ArrayAdapter<String>(requireContext(), com.google.android.material.R.layout.support_simple_spinner_dropdown_item)
 
         incomeGroupViewModel.incomeGroupsLiveData.observe(viewLifecycleOwner) { incomeGroupList ->
@@ -42,10 +49,10 @@ class AddIncomeHistory : Fragment() {
             }
         }
 
-        val spinner = binding.incomeGroupSpinner
-        spinner.adapter = spinnerAdapter
+        val incomeGroupSpinner = binding.incomeGroupSpinner
+        incomeGroupSpinner.adapter = spinnerAdapter
 
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        incomeGroupSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
                 parent: AdapterView<*>,
                 view: View,
@@ -64,10 +71,73 @@ class AddIncomeHistory : Fragment() {
                 Toast.makeText(activity, "Nothing Selected", Toast.LENGTH_LONG).show()
             }
         }
-
-
-
-
     }
+
+    private fun initWalletSpinner() {
+        val spinnerAdapter = ArrayAdapter<String>(requireContext(), com.google.android.material.R.layout.support_simple_spinner_dropdown_item)
+
+        walletViewModel.walletsLiveData.observe(viewLifecycleOwner) { walletList ->
+            walletList?.forEach { it ->
+                spinnerAdapter.add(it.name)
+            }
+        }
+
+        val walletSpinner = binding.walletSpinner
+        walletSpinner.adapter = spinnerAdapter
+
+        walletSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+                val item = binding.walletSpinner.getItemAtPosition(position).toString()
+                Toast.makeText(
+                    requireContext(),
+                    item,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                Toast.makeText(activity, "Nothing Selected", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentAddIncomeHistoryBinding.bind(view)
+
+        val dateET = binding.dateEditText
+
+        val myCalendar = Calendar.getInstance()
+
+        val datePicker = DatePickerDialog.OnDateSetListener() {
+                view, year, month, dayOfMonth ->
+            myCalendar.set(Calendar.YEAR, year)
+            myCalendar.set(Calendar.MONTH,month)
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDate(myCalendar)
+        }
+
+        dateET.setOnClickListener {
+            DatePickerDialog(
+                requireContext(),
+                datePicker,
+                myCalendar.get(Calendar.YEAR),
+                myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+    }
+
+    fun updateDate(calendar: Calendar){
+        val dateFormat = "dd/MM/yy";
+        val sdf = SimpleDateFormat(dateFormat, Locale.US);
+        binding.dateEditText.setText(sdf.format(calendar.time));
+    }
+
 
 }
