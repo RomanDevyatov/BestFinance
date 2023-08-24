6240b52b0092e9c32fb6697a9e9ba00d88f67078
Roman Devyatov <roman_devyatov@epam.com>
2023-08-24 14:51:45 +0300

    added dark theme switcher

--- app/src/main/java/com/romandevyatov/bestfinance/ui/fragments/more/settings/SettingsFragment.kt
+++ app/src/main/java/com/romandevyatov/bestfinance/ui/fragments/more/settings/SettingsFragment.kt
@@ -2,16 +2,19 @@ package com.romandevyatov.bestfinance.ui.fragments.more.settings
 
 import android.content.SharedPreferences
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.fragment.findNavController
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreferenceCompat
 import com.romandevyatov.bestfinance.R
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
-class SettingsFragment : PreferenceFragmentCompat(), OnSharedPreferenceChangeListener {
+class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
@@ -23,31 +26,43 @@ class SettingsFragment : PreferenceFragmentCompat(), OnSharedPreferenceChangeLis
         }
         requireActivity().onBackPressedDispatcher.addCallback(this, callback)
 
-
         // Access preferences and listen for changes
-        val preferenceManager = preferenceManager
-        val sharedPreferences = preferenceManager.sharedPreferences
-        sharedPreferences?.registerOnSharedPreferenceChangeListener(this)
-    }
+//        val preferenceManager = preferenceManager
+//        val sharedPreferences = preferenceManager.sharedPreferences
+//        sharedPreferences?.registerOnSharedPreferenceChangeListener(this)
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-
-        // Unregister the preference change listener
-        preferenceManager.sharedPreferences?.unregisterOnSharedPreferenceChangeListener(this)
-    }
-
-    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
-        when (key) {
-            "pennyCount" -> {
-                val pennyCount = sharedPreferences?.getInt(key, 0)
-                // React to penny count preference change
-            }
-            "fastAI" -> {
-                val fastAIEnabled = sharedPreferences?.getBoolean(key, false)
-                // React to fast AI preference change
-            }
-            // Add more cases for other preferences if needed
+        val darkModePreference = findPreference<SwitchPreferenceCompat>("darkOrLightTheme")
+        darkModePreference?.setDefaultValue(false)
+        darkModePreference?.setOnPreferenceChangeListener { _, newValue ->
+            val isDarkModeEnabled = newValue as Boolean
+            updateTheme(isDarkModeEnabled)
+            true
         }
     }
+
+    private fun updateTheme(isDarkModeEnabled: Boolean) {
+        val nightMode = if (isDarkModeEnabled) {
+            AppCompatDelegate.MODE_NIGHT_YES
+        } else {
+            AppCompatDelegate.MODE_NIGHT_NO
+        }
+        AppCompatDelegate.setDefaultNightMode(nightMode)
+    }
+
+    //    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String?) {
+//        when (key) {
+//            "night_or_light_theme" -> {
+//                val isNightModeEnabled = sharedPreferences.getBoolean(key, false)
+//                setAppTheme(isNightModeEnabled)
+//            }
+//            // Add more cases for other preferences if needed
+//        }
+//    }
+
+//    private fun setAppTheme(isDarkModeEnabled: Boolean) {
+//        val themeId = if (isDarkModeEnabled) R.style.Theme_BestFinance else R.style.Theme_BestFinance
+//        activity?.setTheme(themeId)
+        // You can also apply the theme to the entire application using AppCompatDelegate.setDefaultNightMode()
+        // AppCompatDelegate.setDefaultNightMode(if (isDarkModeEnabled) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO)
+//    }
 }
