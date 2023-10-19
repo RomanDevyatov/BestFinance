f9b4f69f493b9202b1d57dba8c912bc54b1b5fd9
Roman Devyatov <roman_devyatov@epam.com>
2023-10-19 06:13:53 +0300

    using View Binding, analyaze

--- app/src/main/java/com/romandevyatov/bestfinance/ui/adapters/analyze/ExpandableSubGroupAdapter.kt
+++ app/src/main/java/com/romandevyatov/bestfinance/ui/adapters/analyze/ExpandableSubGroupAdapter.kt
@@ -3,96 +3,95 @@ package com.romandevyatov.bestfinance.ui.adapters.analyze
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.RelativeLayout
-import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.romandevyatov.bestfinance.R
+import com.romandevyatov.bestfinance.databinding.CardExpandableParentBinding
 import com.romandevyatov.bestfinance.ui.adapters.analyze.models.ChildData
 import com.romandevyatov.bestfinance.ui.adapters.analyze.models.SubParentData
 import com.romandevyatov.bestfinance.utils.Constants
 
-class ExpandableSubGroupAdapter (private val mList: List<SubParentData>) :
+class ExpandableSubGroupAdapter(private val mList: List<SubParentData>) :
     RecyclerView.Adapter<ExpandableSubGroupAdapter.SubParentItemViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SubParentItemViewHolder {
-        val view: View =
-            LayoutInflater.from(parent.context).inflate(R.layout.card_expandable_parent, parent, false)
-        return SubParentItemViewHolder(view)
+        val binding = CardExpandableParentBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return SubParentItemViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
         return mList.size
     }
 
-    inner class SubParentItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val linearLayout: LinearLayout
-        val expandableLayout: RelativeLayout
-        val mTextView: TextView
-        val summaTextView: TextView
-        val mArrowImage: ImageView
-        val nestedRecyclerView: RecyclerView
+    inner class SubParentItemViewHolder(private val binding: CardExpandableParentBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
         init {
-            linearLayout = itemView.findViewById(R.id.linear_layout)
-            expandableLayout = itemView.findViewById(R.id.expandable_layout)
-            mTextView = itemView.findViewById(R.id.label_global_group)
-            summaTextView = itemView.findViewById(R.id.global_summa_text_view)
-            mArrowImage = itemView.findViewById(R.id.arrow_imageview)
-            nestedRecyclerView = itemView.findViewById(R.id.child_rv)
+            binding.root.setOnClickListener {
+                val subParentData = mList[adapterPosition]
+                subParentData.isExpanded = !subParentData.isExpanded
+                notifyItemChanged(adapterPosition)
+            }
+        }
+
+        fun bind(subParentData: SubParentData) {
+            binding.labelGlobalGroup.text = subParentData.parentTitle
+            binding.globalSummaTextView.text = calculateSubGroupSumma(subParentData).toString()
+
+            if (subParentData.isExpanded) {
+                binding.expandableLayout.visibility = View.VISIBLE
+                binding.arrowImageview.setImageResource(R.drawable.ic_arrow_up)
+            } else {
+                binding.expandableLayout.visibility = View.GONE
+                binding.arrowImageview.setImageResource(R.drawable.ic_arrow_down)
+            }
+
+            when (subParentData.type) {
+                Constants.INCOMINGS_PARENT_TYPE -> {
+                    val adapter = NestedChildAdapter(
+                        subParentData.childNestedListOfIncomeSubGroup?.map {
+                            ChildData(it, null, Constants.INCOMINGS_PARENT_TYPE)
+                        }!!.toList()
+                    )
+                    binding.childRv.adapter = adapter
+                }
+
+                Constants.EXPENSES_PARENT_TYPE -> {
+                    val adapter = NestedChildAdapter(
+                        subParentData.childNestedListOfExpenseSubGroup?.map {
+                            ChildData(null, it, Constants.EXPENSES_PARENT_TYPE)
+                        }!!.toList()
+                    )
+                    binding.childRv.adapter = adapter
+                }
+            }
+
+            binding.childRv.layoutManager = LinearLayoutManager(binding.root.context)
+        }
+
+        private fun calculateSubGroupSumma(subParentData: SubParentData): Double {
+            return when (subParentData.type) {
+                Constants.INCOMINGS_PARENT_TYPE -> {
+                    subParentData.childNestedListOfIncomeSubGroup?.sumOf { incomeSubGroupWithIncomeHistories ->
+                        incomeSubGroupWithIncomeHistories.incomeHistories.sumOf { it.amount }
+                    } ?: 0.0
+                }
+                Constants.EXPENSES_PARENT_TYPE -> {
+                    subParentData.childNestedListOfExpenseSubGroup?.sumOf { expenseSubGroupWithExpenseHistories ->
+                        expenseSubGroupWithExpenseHistories.expenseHistory.sumOf { it.amount }
+                    } ?: 0.0
+                }
+                else -> 0.0
+            }
         }
     }
 
     override fun onBindViewHolder(holder: SubParentItemViewHolder, position: Int) {
         val subParentData: SubParentData = mList[position]
-
-        holder as SubParentItemViewHolder
-
-        holder.mTextView.text = subParentData.parentTitle
-
-        if (subParentData.isExpanded) {
-            holder.expandableLayout.visibility = View.VISIBLE
-            holder.mArrowImage.setImageResource(R.drawable.ic_arrow_up)
-        } else {
-            holder.expandableLayout.visibility = View.GONE
-            holder.mArrowImage.setImageResource(R.drawable.ic_arrow_down)
-        }
-
-        val adapter: NestedChildAdapter
-        when (subParentData.type) {
-            Constants.INCOMINGS_PARENT_TYPE -> {
-                adapter = NestedChildAdapter(subParentData.childNestedListOfIncomeSubGroup?.map {
-                    ChildData(it, null, Constants.INCOMINGS_PARENT_TYPE)
-                }!!.toList())
-                holder.nestedRecyclerView.adapter = adapter
-
-                val subGroupSumma = subParentData.childNestedListOfIncomeSubGroup?.sumOf { incomeSubGroupWithIncomeHistories ->
-                    incomeSubGroupWithIncomeHistories.incomeHistories.sumOf { it.amount }
-                }
-                holder.summaTextView.text = subGroupSumma.toString()
-            }
-
-            Constants.EXPENSES_PARENT_TYPE -> {
-                adapter = NestedChildAdapter(subParentData.childNestedListOfExpenseSubGroup?.map {
-                    ChildData(null, it, Constants.EXPENSES_PARENT_TYPE)
-                }!!.toList())
-                holder.nestedRecyclerView.adapter = adapter
-
-                val subGroupSumma = subParentData.childNestedListOfExpenseSubGroup?.sumOf { expenseSubGroupWithExpenseHistories ->
-                    expenseSubGroupWithExpenseHistories.expenseHistory.sumOf { it.amount }
-                }
-                holder.summaTextView.text = subGroupSumma.toString()
-            }
-        }
-
-        holder.nestedRecyclerView.layoutManager = LinearLayoutManager(holder.itemView.context)
-
-
-        holder.linearLayout.setOnClickListener {
-            subParentData.isExpanded = !subParentData.isExpanded
-            notifyItemChanged(holder.adapterPosition)
-        }
+        holder.bind(subParentData)
     }
 }
