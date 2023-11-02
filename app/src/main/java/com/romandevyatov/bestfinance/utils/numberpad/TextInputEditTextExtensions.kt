a6119e0c9bf943c3b1bad2cd7ac41a192403cf58
Roman Devyatov <roman_devyatov@epam.com>
2023-11-02 19:33:10 +0300

    fixing added number pad for number input edit texts

--- app/src/main/java/com/romandevyatov/bestfinance/utils/numberpad/TextInputEditTextExtensions.kt
+++ app/src/main/java/com/romandevyatov/bestfinance/utils/numberpad/TextInputEditTextExtensions.kt
@@ -28,21 +28,22 @@ fun TextInputEditText.addGenericTextWatcher() {
 private fun processInput(text: String): String {
     var updatedText = text.replace(",", ".").replace("-", "0")
 
-    val decimalCount = updatedText.count { it == '.' }
-    if (decimalCount > 1) {
-        updatedText = updatedText.replaceFirst(".", "")
-    }
-
     if (updatedText.length > 1) {
-      if (updatedText.count { it == '0' } > 1) {
-          return updatedText.replaceFirst("0", "")
-      }
-      if (updatedText[0] == '0') {
-          val secondChar = updatedText[1]
-          if (secondChar in '1'..'9') {
-              return secondChar + updatedText.substring(2)
-          }
-      }
+        if (updatedText.count { it == '.' } > 1) {
+            return updatedText.replaceFirst(".", "")
+        }
+
+        if (updatedText[0] == '0') {
+            if (updatedText.count { it == '0' } > 1) {
+                return updatedText.replaceFirst("0", "")
+            } else {
+
+                val secondChar = updatedText[1]
+                if (secondChar in '1'..'9') {
+                    return secondChar + updatedText.substring(2)
+                }
+            }
+        }
     }
 
     return updatedText
